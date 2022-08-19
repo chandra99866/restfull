33e5058c0fb29338b917908c8ebae7b73570b2d1
Chandrasekhar.R <chandrasekhar.r@molecularconnections.com>
2022-08-19 19:37:12 +0530

    added demo api and made 8080 port as static

---
+++ src/main/java/com/example/restfull/restfull/controler/Demo.java
@@ -0,0 +1,25 @@
+package com.example.restfull.restfull.controler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class Demo {
+
+
+    @GetMapping("/demo")
+    public ResponseEntity<Object> demo(){
+        Map<String,String> demo = new HashMap<>();
+        demo.put("Access","GRANTED");
+        demo.put("Demo","SUCCESS");
+        demo.put("Time", new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date()));
+        return new ResponseEntity<>(demo, HttpStatus.OK);
+    }
+}
