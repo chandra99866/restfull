9d6c0897f6cadfe9d325394d135cf1d76dfb0c0b
Chandrasekhar.R <chandrasekhar.r@molecularconnections.com>
2022-09-26 22:10:58 +0530

    extra date variable is removed

--- src/main/java/com/example/restfull/restfull/entity/User.java
+++ src/main/java/com/example/restfull/restfull/entity/User.java
@@ -12,55 +12,51 @@ import java.util.Date;
 @Data
 public class User {
 	
-    @Id
+	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int user_id ;
     
     @Column(unique = true)
-   private String userName;
+    private String userName;
     
     
-   private int userAge;
+    private int userAge;
 
-   @Temporal(TemporalType.TIMESTAMP)
-   private Date createdDate;
+    private Date createdDate;
+    
+    
 
-    public Date getDate() {
-        return createdDate;
-    }
-
-    public void setDate(Date date) {
-        this.createdDate = date;
-    }
-
-    public int getUser_id() {
-	return user_id;
-
-   }
-
-   public void setUser_id(int user_id) {
-	this.user_id = user_id;
-
-   }
-
-   public String getUserName() {
-	return userName;
-
-   }
-
-   public void setUserName(String userName) {
-	this.userName = userName;
-
-   }
-
-   public int getUserAge() {
-	return userAge;
+	public int getUser_id() {
+		return user_id;
 	}
 
-   public void setUserAge(int userAge) {
-	this.userAge = userAge;
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
 
-   }
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getUserAge() {
+		return userAge;
+	}
+
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
 
 
 }
