94363817c00e6a537fe2fdf68a27ccede13ae2e4
Chandrasekhar.R <chandrasekhar.r@molecularconnections.com>
2022-10-14 09:47:37 +0530

    added only required api paths for Swaggerconfig

--- src/main/java/com/example/restfull/restfull/config/Swaggerconfig.java
+++ src/main/java/com/example/restfull/restfull/config/Swaggerconfig.java
@@ -8,7 +8,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,7 +23,7 @@ public class Swaggerconfig {
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
           .apis(RequestHandlerSelectors.any())   
-          .paths(PathSelectors.any())   
+          .paths(PathSelectors.regex("/first.*"))
           .build()
           .apiInfo(apiInfo());
     }
